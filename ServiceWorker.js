f62f92bf7874a99d8067869df55c445ff12758ba
Psajmon <84076660+Psajmon@users.noreply.github.com>
2025-08-12 13:59:36 +0200

    try to fix audiopeer

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "XRCOO-Audio Visualizer Volcano FX-0.1.0";
+const contentToCache = [
+    "Build/AudioFX.loader.js",
+    "Build/AudioFX.framework.js",
+    "Build/AudioFX.data",
+    "Build/AudioFX.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
